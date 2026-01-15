175b2553f60146529150a56d3ab165f8b274a7b4
suraj mishra <surajmishra574619@gmail.com>
2026-01-15 18:08:29 +0530

    Parking Ticket Entity Added

--- parkinglot/src/main/java/org/example/entities/ParkingTicket.java
+++ parkinglot/src/main/java/org/example/entities/ParkingTicket.java
@@ -1,4 +1,45 @@
 package org.example.entities;
 
+import java.util.Date;
+import java.util.UUID;
+
 public class ParkingTicket {
+    private final String parkingId;
+    private final long entryTime;
+    private long exitTime;
+    private final Vehicle vehicle;
+    private final ParkingSpot parkingSpot;
+
+    public ParkingTicket(Vehicle vehicle, ParkingSpot parkingSpot) {
+        this.parkingId = UUID.randomUUID().toString();
+        this.entryTime = new Date().getTime();
+        this.vehicle = vehicle;
+        this.parkingSpot = parkingSpot;
+    }
+
+    public String getParkingId() {
+        return parkingId;
+    }
+
+    public long getEntryTime() {
+        return entryTime;
+    }
+
+    public long getExitTime() {
+        return exitTime;
+    }
+
+    public void setExitTime() {
+        this.exitTime = new Date().getTime();
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public ParkingSpot getParkingSpot() {
+        return parkingSpot;
+    }
 }
+
+
